a12bd9f16b8bb1e908ded7d45024f3df57e65b60
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-06-04 07:46:57 -0500

    Start thought routes

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-const { User } = require('./models');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialdb',
@@ -19,6 +20,8 @@ mongoose.connect(
 );
 
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
